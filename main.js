8f277087540cbe913d67abf725fb83c5dcb0b908
Oleksii <alexeylom22@gmail.com>
2020-02-21 21:45:05 +0200

    this is my first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+﻿"use strict";
+
+
+console.log(2/0);
+console.log("string"*9);
+
+let something;
+console.log(something);
+
+let person= {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ["plum.png", "orange.jpeg", "apple.bmp"];
+
+console.log(arr[0]);
+
+//alert("Hello World");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+// let answer = prompt("Есть ли Вам 18?", "Да");
+// console.log(typeof(arr));
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
+
+
+alert("Test git");
+
+
+
